026ede982ba3a582fce9a2ee4f257c2eaa18854b
Freddy Iñiguez <iniguez.devt@gmail.com>
2017-08-10 12:39:41 -0500

    List of projects and navigation were added.

--- tool/urls.py
+++ tool/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home),
-	url(r'^milestones/', views.milestones),
-	url(r'^projects/', views.projects),
+	url(r'^milestones/$', views.milestones),
+	url(r'^projects/$', views.projects),
+	url(r'^projects/(?P<pk>\d+)/$', views.project_detail, name = 'project_detail'),
+	url(r'^projects/new/$', views.project_new, name = 'project_new'),
+	url(r'^help/', views.help),
 ]
--- tool/views.py
+++ tool/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
+
+# - - - H O M E - - -
 # Home page
 def home(request):
 	return render(request, 'tool/index.html', {})
 
+
+# - - - M I L E S T O N E S - - - 
 # Milestones and business information about SMART-SPI
 def milestones(request):
 	return render(request, 'tool/milestones.html', {})
 
+
+# - - - P R O J E C T S - - - 
 # Projects creation, capture of process information and data analysis and visualization
 def projects(request):
-	return render(request, 'tool/projects.html', {})
+	projects = Project.objects.all()
+	return render(request, 'tool/projects.html', {'projects' : projects})
+
+# Returns a project details
+def project_detail(request, pk):
+	project = get_object_or_404(Project, pk=pk)
+	return render(request, 'tool/project_detail.html', {'project' : project})
+
+# Creates a new project
+def project_new(request):
+	return render(request, 'tool/project_new.html', {})
+
+
+# - - - T E M P O R A L - - - 
+# A temporal link to see the available HTML theme elements
+def help(request):
+	return render(request, 'tool/help.html', {})
